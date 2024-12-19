b84150d104f65e5c67b1aedbb406dbf613247422
BarisTay <baristay94@gmail.com>
2024-12-19 23:14:42 +0300

    Added cinemachine package, scene and a playable character

--- Assets/Scripts/MyScript.cs
+++ Assets/Scripts/MyScript.cs
@@ -1,9 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class MyScript : MonoBehaviour
+public class Movement : MonoBehaviour
 {
+    public float speed = 3f;
+    public float sprintSpeed = 1.75f;
+    public Transform cameraTransform;
+    public Transform playerTransform;
+    float xMove, yMove;
+    float gravity = -9.81f;
+    Vector3 movement;
+    CharacterController controller;
     void Start()
     {
-        Debug.Log("This is just a log.");
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        xMove = Input.GetAxis("Horizontal") * speed;
+        yMove = Input.GetAxis("Vertical") * speed;
+        if (Keyboard.current.leftShiftKey.isPressed)
+        {
+            yMove *= sprintSpeed;
+            Debug.Log(xMove);
+        }
+        movement = new Vector3(xMove, gravity, yMove);
+        Quaternion targetRotation = Quaternion.Euler(0f, cameraTransform.rotation.eulerAngles.y, 0f);
+        movement *= speed;
+        movement = targetRotation * movement;
+        playerTransform.rotation = Quaternion.Euler(0f, cameraTransform.rotation.eulerAngles.y, 0f);
+        controller.Move(movement * Time.deltaTime);
     }
 }
