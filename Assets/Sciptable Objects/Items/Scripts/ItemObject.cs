24fbd6f8c3f43197fbbe9435d6525566f5862816
BarisTay <baristay94@gmail.com>
2025-01-27 15:36:07 +0300

    Inventory demo for interacting

---
+++ Assets/Sciptable Objects/Items/Scripts/ItemObject.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public enum ItemType
+{
+    Food,
+    Equipment,
+    Default
+}
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public ItemType type;
+    [TextArea(15,20)]
+    public string description;
+}
